274c142b009fcd60102bd337614f8d7870104ff9
shubham durgade <shubhamdurgade1203@gmail.com>
2025-10-01 19:16:40 +0530

    Removed Unwanted Code

--- SysMatrix/Collector/CpuCollector.cs
+++ SysMatrix/Collector/CpuCollector.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System; 
 using System.Threading.Tasks; 
-using System.Diagnostics;
-using System.Threading.Tasks;
+using System.Diagnostics; 
 using SysMatrix.Models;
 namespace SysMatrix.Collector
 {
